b7da94345d07b45a51c5f94629a149c0694360d3
helio rivera <sorencen@gmail.com>
2021-10-20 21:57:43 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GRU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Red/GRU/Gru.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRU.Red
+{
+    class Gru
+    {
+        List<double> celdaDeEstado;
+        public Gru()
+        {
+            celdaDeEstado = new List<double>();
+
+
+        }
+    }
+}

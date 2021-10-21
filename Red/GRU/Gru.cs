6e0ad9141f529e7a2ce7d2a0b4e108114a812d09
helio rivera <sorencen@gmail.com>
2021-10-21 13:05:16 -0300

    Incorporacion de clase capa, clase neurona, agregacion de metodo sigmoide en clase gru.

--- Red/GRU/Gru.cs
+++ Red/GRU/Gru.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GRU.Red.GRU.LogisticRegressionNeurons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,26 @@ namespace GRU.Red
 {
     class Gru
     {
-        List<double> celdaDeEstado;
+        
+
         public Gru()
         {
-            celdaDeEstado = new List<double>();
-
-
+           
         }
+
+        public List<double> Sigmoide(Capa unaCapa)
+        {
+            List<double> salidaSigmoide = new List<double>();
+
+            List<double> salidasDeCadaNeuronaEnCapa = unaCapa.GetSalida();
+           
+            for (int i =0; i< unaCapa.getTamanoSalida() ;++i)
+            {
+                salidaSigmoide.Add(1 / (1 + Math.Exp(salidasDeCadaNeuronaEnCapa[i])));          
+            }
+
+            return salidaSigmoide;
+        }
+        
     }
 }
